5cb926d129614d190915407fa580fc1848096e28
Felipe Dominguez <fel@reactbaby.local>
2018-05-30 16:05:55 -0700

    moving from BrowserRouter to Hashrouter for gh pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import Main from "./Main/Main";
 
@@ -28,9 +28,9 @@ class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <Main />
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
